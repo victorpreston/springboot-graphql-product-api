d39da0f379d3bddf144ee1203d82d9d1333900a0
Victor Preston <prestonvictor25@gmail.com>
2026-02-16 00:30:27 +0300

    project starter

---
+++ springboot-graphql-product-api/src/main/java/com/example/springboot_graphql_product_api/SpringbootGraphqlProductApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot_graphql_product_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGraphqlProductApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootGraphqlProductApiApplication.class, args);
+	}
+
+}
