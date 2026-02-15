931a4551f8634c3e0784d8a2e0273a55684cbc0b
Victor Preston <prestonvictor25@gmail.com>
2026-02-16 02:35:44 +0300

    feat: add product enums (Category, Status, SortBy)

---
+++ src/main/java/com/example/springboot_graphql_product_api/enums/ProductSortBy.java
@@ -0,0 +1,12 @@
+package com.example.springboot_graphql_product_api.enums;
+
+public enum ProductSortBy {
+    NAME_ASC,
+    NAME_DESC,
+    PRICE_ASC,
+    PRICE_DESC,
+    CREATED_AT_ASC,
+    CREATED_AT_DESC,
+    STOCK_ASC,
+    STOCK_DESC
+}
---
+++ src/main/java/com/example/springboot_graphql_product_api/enums/ProductStatus.java
@@ -0,0 +1,8 @@
+package com.example.springboot_graphql_product_api.enums;
+
+public enum ProductStatus {
+    ACTIVE,
+    INACTIVE,
+    DISCONTINUED,
+    OUT_OF_STOCK
+}
